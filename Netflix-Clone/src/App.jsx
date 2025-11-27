b1b67d4ec6bd3c1e0fc301c40e5a7a18a166a859
Moges741 <mogesse741@gamil.com>
2025-11-28 01:04:37 +0300

    Header and Footer

--- Netflix-Clone/src/App.jsx
+++ Netflix-Clone/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 
 import './App.css'
+import Home from './Pages/Home/Home';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <h1 className='app'>Hello There</h1>
+  <Home/>
   );
 }
 
