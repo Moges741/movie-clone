09edc196df2c230b56de274eb63aefaa4c63ebb9
Moges741 <mogesse741@gamil.com>
2025-11-29 22:47:36 +0300

    Comment all Header Part, add more styles and add row and RowLists

--- Netflix-Clone/src/Components/Header/Header.jsx
+++ Netflix-Clone/src/Components/Header/Header.jsx
@@ -1,47 +1,42 @@
-import React from "react";
-import './header.css'
-import NetflixLogo from '../../assets/images/Netflix_2015_logo.svg.png';
-import SearchIcon from "@mui/icons-material/Search";
-import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
-const Header = () => {
-  return (
-    <div className="header-outer-container">
-      <div className="header-container">
-        <div className="header-left">
-          <ul>
-            <li>
-              <img
-                className="netflix-logo"
-                src={NetflixLogo}
-                alt="Netflx Logo "
-                width="100"
-              />
-            </li>
-            <li>Home</li>
-            <li>TVShow</li>
-            <li>Movies</li>
-            <li>Latest</li>
-            <li>MyList</li>
-            <li>Browse by Languages</li>
-          </ul>
-        </div>
-        <div className="header-right">
-          <ul>
-            <li><SearchIcon/></li>
-            <li><NotificationsNoneIcon/></li>
-            <li><AccountBoxIcon/></li>
-            <li><ArrowDropDownIcon/></li>
-          </ul>
+// import './header.css'
+// import NetflixLogo from '../../assets/images/Netflix_2015_logo.svg.png';
+// import SearchIcon from "@mui/icons-material/Search";
+// import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+// import AccountBoxIcon from '@mui/icons-material/AccountBox';
+// import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+// const Header = () => {
+//   return (
+//     <div className="header-outer-container">
+//       <div className="header-container">
+//         <div className="header-left">
+//           <ul>
+//             <li>
+//               <img
+//                 className="netflix-logo"
+//                 src={NetflixLogo}
+//                 alt="Netflx Logo "
+//                 width="100"
+//               />
+//             </li>
+//             {/* <li>Home</li>
+//             <li>TVShow</li>
+//             <li>Movies</li>
+//             <li>Latest</li>
+//             <li>MyList</li>
+//             <li>Browse by Languages</li> */}
+//           </ul>
+//         </div>
+//         <div className="header-right">
+//           <ul>
+//             <li><SearchIcon/></li>
+//             <li><NotificationsNoneIcon/></li>
+//             <li><AccountBoxIcon/></li>
+//             <li><ArrowDropDownIcon/></li>
+//           </ul>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-
-
-
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Header;
+// export default Header;
